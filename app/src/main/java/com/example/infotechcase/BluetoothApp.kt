f08e6c52e984306e7dd3ffd6f42652f12521b441
Haggai Khafizov <hafyok5777@gmail.com>
2024-07-24 16:28:50 +0300

    Add Bluetooth in app

---
+++ app/src/main/java/com/example/infotechcase/BluetoothApp.kt
@@ -0,0 +1,90 @@
+package com.example.infotechcase
+
+import android.Manifest
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+
+@Composable
+fun BluetoothApp() {
+    val context = LocalContext.current
+    val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    val bluetoothAdapter = bluetoothManager.adapter
+    var bluetoothStatus by remember { mutableStateOf("Checking Bluetooth...") }
+    var hasPermission by remember { mutableStateOf(false) }
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        hasPermission = isGranted
+    }
+
+    LaunchedEffect(Unit) {
+        when {
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                hasPermission = true
+            }
+            else -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    requestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
+                }
+            }
+        }
+    }
+
+    Column(modifier = Modifier.fillMaxSize().padding(32.dp)) {
+        if (bluetoothAdapter != null) {
+            if (hasPermission) {
+                if (bluetoothAdapter.isEnabled) {
+                    val myDeviceAddress = bluetoothAdapter.address
+                    val myDeviceName = bluetoothAdapter.name
+                    bluetoothStatus = "$myDeviceName : $myDeviceAddress"
+                } else {
+                    bluetoothStatus = "Bluetooth выключен"
+                }
+            } else {
+                bluetoothStatus = "Нет разрешений"
+            }
+        } else {
+            bluetoothStatus = "Bluetooth не поддерживается"
+        }
+
+        Text(text = bluetoothStatus)
+
+        Button(onClick = {
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    requestPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
+                }
+            } else {
+                hasPermission = true
+            }
+        }) {
+            Text(text = "Запросить разрешение")
+        }
+    }
+}
